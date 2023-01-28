e3199d24bb6731d8c22ebc5048d26e6e29c78d17
Rafael Malanco <rafael.malanco@softtek.com>
2023-01-27 20:06:21 -0600

    XD

--- cursowebrmva03website/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace cursowebrmva03website.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
